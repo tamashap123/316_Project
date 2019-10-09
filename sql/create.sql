aa67af9dbca408ab729f50edc29aac3fd25fc3ef
Jonathon Kuo <jonathon116@gmail.com>
2019-10-09 18:01:54 -0400

    create edits

--- sql/create.sql
+++ sql/create.sql
@@ -3,10 +3,11 @@ CREATE TABLE Congressman
  name VARCHAR(256) NOT NULL,
  house_or_senate CHAR(1) NOT NULL,
  state VARCHAR(2) NOT NULL,
- district INTEGER NOT NULL,
+ district INTEGER,
  party CHAR(1) NOT NULL,
  CHECK(house_or_senate in ('H', 'S')),
- CHECK(party in ('D', 'R', 'I')));
+ CHECK(party in ('D', 'R', 'I')),
+ CHECK(district IS NOT NULL OR house_or_senate = 'S'));
 
 CREATE TABLE Bill
 (type CHAR(1) NOT NULL,
@@ -21,7 +22,7 @@ CREATE TABLE Bill
  CHECK(cong_year < 116));
 
 CREATE TABLE SponsoredBy
-(bill_type VARCHAR(5) NOT NULL,
+(bill_type CHAR(1) NOT NULL,
  bill_num INTEGER NOT NULL,
  rep_id INTEGER NOT NULL REFERENCES Congressman(id),
  PRIMARY KEY(bill_type, bill_num, rep_id),
@@ -29,7 +30,7 @@ CREATE TABLE SponsoredBy
 
 CREATE TABLE Vote
 (rep_id INTEGER NOT NULL REFERENCES Congressman(id),
- bill_type VARCHAR(5) NOT NULL,
+ bill_type CHAR(1) NOT NULL,
  bill_num INTEGER NOT NULL,
  decision VARCHAR(10) NOT NULL,
  PRIMARY KEY(rep_id, bill_type, bill_num),
@@ -49,4 +50,25 @@ CREATE TABLE RepresentedBy
 
 -- TODO: 
 -- - trigger on RegisteredUser that updates RepresentedBy?
--- - trigger on Bill that updates SponsoredBy?
+
+CREATE FUNCTION F_Get_Representatives() RETURNS TRIGGER AS $$
+BEGIN
+ IF (TG_OP = 'INSERT') THEN
+ 	-- INSERT INTO RepresentedBy (
+ 	-- 	SELECT RegisteredUser.email, Congressman.id
+ 	-- 	FROM Congressman, RegisteredUser
+ 	-- 	WHERE RegisteredUser.email = NEW.email 
+ 	-- 	AND ((Congressman.house_or_senate = 'S' AND Congressman.state = RegisteredUser.state) 
+ 	-- 		OR (Congressman.house_or_senate = 'H' AND Congressman.state = RegisteredUser.state AND Congressman.district = RegisteredUser.district))
+ 	-- );
+ END IF;
+ IF (TG_OP = 'UPDATE') THEN
+
+ END IF;
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER T_Get_Representatives
+ AFTER INSERT OR UPDATE ON RegisteredUser
+ EXECUTE PROCEDURE F_Get_Representatives();
