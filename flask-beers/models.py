16220d6898c46a5c254578088597393cdec33336
Tamasha Pathirathna <tsp18@duke.edu>
2019-11-04 23:51:36 -0500

    models updated with anna's input

--- flask-beers/models.py
+++ flask-beers/models.py
@@ -1,75 +1,6 @@
 from sqlalchemy import sql, orm
 from app import db
 
-# class Drinker(db.Model):
-#     __tablename__ = 'drinker'
-#     name = db.Column('name', db.String(20), primary_key=True)
-#     address = db.Column('address', db.String(20))
-#     likes = orm.relationship('Likes')
-#     frequents = orm.relationship('Frequents')
-#     @staticmethod
-#     def edit(old_name, name, address, beers_liked, bars_frequented):
-#         try:
-#             db.session.execute('DELETE FROM likes WHERE drinker = :name',
-#                                dict(name=old_name))
-#             db.session.execute('DELETE FROM frequents WHERE drinker = :name',
-#                                dict(name=old_name))
-#             db.session.execute('UPDATE drinker SET name = :name, address = :address'
-#                                ' WHERE name = :old_name',
-#                                dict(old_name=old_name, name=name, address=address))
-#             for beer in beers_liked:
-#                 db.session.execute('INSERT INTO likes VALUES(:drinker, :beer)',
-#                                    dict(drinker=name, beer=beer))
-#             for bar, times_a_week in bars_frequented:
-#                 db.session.execute('INSERT INTO frequents'
-#                                    ' VALUES(:drinker, :bar, :times_a_week)',
-#                                    dict(drinker=name, bar=bar,
-#                                         times_a_week=times_a_week))
-#             db.session.commit()
-#         except Exception as e:
-#             db.session.rollback()
-#             raise e
-
-# class Beer(db.Model):
-#     __tablename__ = 'beer'
-#     name = db.Column('name', db.String(20), primary_key=True)
-#     brewer = db.Column('brewer', db.String(20))
-
-# class Bar(db.Model):
-#     __tablename__ = 'bar'
-#     name = db.Column('name', db.String(20), primary_key=True)
-#     address = db.Column('address', db.String(20))
-#     serves = orm.relationship('Serves')
-
-# class Likes(db.Model):
-#     __tablename__ = 'likes'
-#     drinker = db.Column('drinker', db.String(20),
-#                         db.ForeignKey('drinker.name'),
-#                         primary_key=True)
-#     beer = db.Column('beer', db.String(20),
-#                      db.ForeignKey('beer.name'),
-#                      primary_key=True)
-
-# class Serves(db.Model):
-#     __tablename__ = 'serves'
-#     bar = db.Column('bar', db.String(20),
-#                     db.ForeignKey('bar.name'),
-#                     primary_key=True)
-#     beer = db.Column('beer', db.String(20),
-#                      db.ForeignKey('beer.name'),
-#                      primary_key=True)
-#     price = db.Column('price', db.Float())
-
-# class Frequents(db.Model):
-#     __tablename__ = 'frequents'
-#     drinker = db.Column('drinker', db.String(20),
-#                         db.ForeignKey('drinker.name'),
-#                         primary_key=True)
-#     bar = db.Column('bar', db.String(20),
-#                     db.ForeignKey('bar.name'),
-#                     primary_key=True)
-#     times_a_week = db.Column('times_a_week', db.Integer())
-
 class Bill(db.Model):
     __tablename__ = 'bill'
     type = db.Column('type', db.String(2), primary_key=True)
@@ -95,9 +26,47 @@ class Congressman(db.Model):
     address = db.Column('address', db.String(256))
     contact_form = db.Column('contact_form', db.String(256))
 
+class SponsoredBy(db.Model):
+    __tablename__ = 'SponsoredBy'
+    bill_type = db.Column('bill_type',db.String(2),
+                            db.ForeignKey('bill.type'),
+                            primary_key=True)
+    bill_num = db.Column('bill_num',db.Integer(),
+                            db.ForeignKey('bill.num'),
+                            primary_key=True)
+    cong_year = db.Column('cong_year',db.Integer(),
+                            db.ForeignKey('bill.cong_year'),
+                            primary_key=True)
+    rep_id = db.Column('rep_id',db.String(),
+                            primary_key=True)
 
+class Vote(db.Model):
+    __tablename__ = 'vote'
+    rep_id = db.Column('rep_id', db.String(15),
+                            primary_key=True)
+    bill_type = db.Column('bill_type', db.String(2),
+                            db.ForeignKey('bill.type'),
+                            primary_key=True)
+    bill_num = db.Column('bill_num', db.Integer(),
+                            db.ForeignKey('bill.num'),
+                            primary_key=True)
+    cong_year = db.Column('cong_year', db.Integer(),
+                            db.ForeignKey('bill.cong_year'),
+                            primary_key=True)
+    decision = db.Column('decision', db.String(10))
 
+class RegisteredUser(db.Model):
+    __tablename__ = 'registereduser'
+    email = db.Column('email', db.String(256), primary_key = True)
+    name = db.Column('name', db.String(256))
+    state = db.Column('state', db.String(2))
+    district = db.Column('district', db.Integer())
 
-
-
-
+class RepresentedBy
+    __tablename__ = 'representedby'
+    email = db.Column('email', db.String(256), 
+                    db.ForeignKey('registereduser.email'),
+                    primary_key=True)
+    rep_id = db.Column('rep', db.String(256), 
+                    db.ForeignKey('congressman.id'),
+                    primary_key=True)
