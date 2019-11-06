1de5dd90e142eec3c0fa863dfa9bd8f223e37208
Qingning Zhang <qz54@duke.edu>
2019-11-05 21:43:00 -0500

    test-real-sample done

--- sql/test-real-sample.sql
+++ sql/test-real-sample.sql
@@ -64,8 +64,8 @@ FROM Vote v
 WHERE v.rep_id in (SELECT rep_id FROM RepresentedBy WHERE email='xiaomingwang@gmail.com')
 GROUP BY v.bill_type, v.bill_num, v.cong_year;
 
--- 8.b. Tally the numbers of yeas/nays/abstains on a certain bill across the years.
--- ! Inputs/Implied from user: bill type, bill num, vote status
+-- 8.b. Tally the number of yeas/nays/abstains on a certain bill across the years.
+-- ! Inputs from user: bill type, bill num
 SELECT max(b.category) as category, v.cong_year, b.enacted, 
 	sum(case when v.decision = 'Yea' then 1 else 0 end) AS YeaCount,
 	sum(case when v.decision = 'Nay' then 1 else 0 end) AS NayCount,
@@ -75,3 +75,17 @@ FROM Vote v, Bill b
 WHERE b.type=v.bill_type and b.num=v.bill_num and v.cong_year=b.cong_year and
 v.bill_type='hr' and v.bill_num=2217 
 GROUP BY v.cong_year, b.enacted;
+
+-- 9. Compare two representatives' votes for bills that both voted.
+-- ! Inputs from user: two representatives' ids (from dropdown menu that show their names)
+SELECT A.bill_type, A.bill_num, A.cong_year, A.decision as Butterfield, B.decision as Harris
+FROM ((SELECT v1.bill_type, v1.bill_num, v1.cong_year, v1.decision, c1.name FROM Vote v1, Congressman c1 WHERE v1.rep_id=c1.id and c1.name='G. K. Butterfield') A 
+	JOIN (SELECT v2.bill_type, v2.bill_num, v2.cong_year, v2.decision, c2.name FROM Vote v2, Congressman c2 WHERE v2.rep_id=c2.id and c2.name='Kamala D. Harris') B
+	ON A.bill_type=B.bill_type and A.bill_num=B.bill_num and A.cong_year=B.cong_year)
+ORDER BY A.bill_type, A.bill_num, A.cong_year;
+
+-- 10. Display bills that are introduced after a certain date.
+-- ! Inputs from user: a string in YYYY-MM-DD form
+SELECT * FROM Bill b
+WHERE introduction_date>DATE(CAST('2010-03-23' AS VARCHAR))
+ORDER BY b.cong_year, b.type, b.num;
