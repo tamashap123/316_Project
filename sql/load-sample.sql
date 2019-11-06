d530397fc784240f169895c36ae4825c196ddfdd
Qingning Zhang <qz54@duke.edu>
2019-11-05 20:36:32 -0500

    updates on test-real-sample and load-sample

--- sql/load-sample.sql
+++ sql/load-sample.sql
@@ -3,6 +3,7 @@
 INSERT INTO Congressman VALUES('B001135', 'Richard Burr', 'sen', 'NC', NULL, 'Republican', '202-224-3154', '217 Russell Senate Office Building Washington DC 20510', 'https://www.burr.senate.gov/contact/email');
 INSERT INTO Congressman VALUES('B001251', 'G. K. Butterfield', 'rep', 'NC', 1, 'Democrat', '202-225-3101', '2080 Rayburn House Office Building; Washington DC 20515-3301', NULL);
 INSERT INTO Congressman VALUES('H001075', 'Kamala D. Harris', 'sen', 'CA', NULL, 'Democrat', '202-224-3553', '112 Hart Senate Office Building Washington DC 20510', 'https://www.harris.senate.gov/contact');
+INSERT INTO Congressman VALUES('B201911', 'Harry Kam', 'sen', 'NY', NULL, 'Democrat', '715-224-3553', '11th Street NY', 'https://www.harry.senate.gov/contact');
 INSERT INTO Bill VALUES('hr', 2217, 113, TRUE, 'Affordable Care Act...', 'Health', '2010-03-23');
 INSERT INTO Bill VALUES('hr', 2218, 113, TRUE, 'Another act...', 'Health', '2010-03-25');
 INSERT INTO Bill VALUES('s', 2218, 115, TRUE, 'A third act...', 'Economics', '2014-10-23');
@@ -10,6 +11,7 @@ INSERT INTO SponsoredBy VALUES('hr', 2217, 113, 'B001251');
 INSERT INTO SponsoredBy VALUES('hr', 2218, 113, 'H001075');
 INSERT INTO Vote VALUES('B001135', 'hr', 2217, 113, 'Nay');
 INSERT INTO Vote VALUES('H001075', 'hr', 2217, 113, 'Yea');
+INSERT INTO Vote VALUES('B201911', 'hr', 2217, 113, 'Abstain');
 INSERT INTO Vote VALUES('B001251', 'hr', 2217, 113, 'Yea');
 INSERT INTO Vote VALUES('H001075', 'hr', 2218, 113, 'Yea');
 INSERT INTO Vote VALUES('B001251', 'hr', 2218, 113, 'Nay');
--- sql/test-real-sample.sql
+++ sql/test-real-sample.sql
@@ -25,6 +25,7 @@ WHERE s.rep_id=c.id and s.bill_type = b.type and s.bill_num = b.num and s.cong_y
 and c.name='Kamala D. Harris';
 
 -- 4.b. Display all sponsorships by searching a partial name of a reprensentative (allowing vague search).
+-- ! Inputs from user: representative name
 INSERT INTO Congressman VALUES('H001099', 'Jane Harris Doe', 'sen', 'CA', NULL, 'Democrat', '202-124-3553', 'Somewhere else', 'https://www.jdharris.senate.gov/contact');
 INSERT INTO SponsoredBy VALUES('hr', 2217, 113, 'H001099');
 
@@ -33,14 +34,34 @@ WHERE s.rep_id=c.id and s.bill_type = b.type and s.bill_num = b.num and s.cong_y
 and c.name like '%Harris%';
 
 -- 5.a. Display all votes by searching the exact full name of a representative (using a dropdown menu/autofill).
+-- ! Inputs from user: representative name
 SELECT b.type, b.num, b.cong_year, b.summary, b.category, v.decision FROM Vote v, Congressman c, Bill b
 WHERE v.rep_id=c.id and v.bill_type = b.type and v.bill_num = b.num and v.cong_year = b.cong_year
 and c.name='G. K. Butterfield';
 
 -- 5.b. Display all votes by searching a partial name of a reprensentative (allowing vague search).
+-- ! Inputs from user: representative name
 INSERT INTO Congressman VALUES('H021299', 'Butterfield Harris Doe', 'sen', 'CA', NULL, 'Democrat', '202-124-3553', 'Somewhere else', 'https://www.jdharris.senate.gov/contact');
 INSERT INTO Vote VALUES('H021299', 'hr', 2217, 113, 'Abstain');
 
 SELECT c.name, b.type, b.num, b.cong_year, b.summary, b.category, v.decision FROM Vote v, Congressman c, Bill b
 WHERE v.rep_id=c.id and v.bill_type = b.type and v.bill_num = b.num and v.cong_year = b.cong_year
 and c.name like '%Butterfield%';
+
+-- 6. Display all representatives who voted a certain way on a bill, e.g. voted Yea on bill hr2217 in session 113. 
+-- ! Inputs from user: bill type, bill num, congress year, vote status
+SELECT c.name FROM Vote v, Congressman c
+WHERE v.rep_id=c.id and 
+v.bill_type='hr' and v.bill_num=2217 and v.cong_year=113 and v.decision='Yea';
+
+-- 7. Display count of votes on bills casted by representatives (house and senate) representing a user.
+-- ! Inputs/Implied from user: email
+
+SELECT v.bill_type, v.bill_num, v.cong_year, 
+	sum(case when v.decision = 'Yea' then 1 else 0 end) AS YeaCount,
+	sum(case when v.decision = 'Nay' then 1 else 0 end) AS NayCount,
+	sum(case when v.decision = 'Abstain' then 1 else 0 end) AS AbstainCount
+FROM Vote v
+WHERE v.rep_id in (SELECT rep_id FROM RepresentedBy WHERE email='xiaomingwang@gmail.com')
+GROUP BY v.bill_type, v.bill_num, v.cong_year;
+
